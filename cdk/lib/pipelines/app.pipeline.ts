abd39b2e59dfac673680f3156f6aefc3af0f0136
Declan Price <declanprice@gmail.com>
2024-07-25 13:38:03 +1000

    initial cdk and next.js projects

---
+++ cdk/lib/pipelines/app.pipeline.ts
@@ -0,0 +1,8 @@
+import {Stack} from "aws-cdk-lib";
+import {Construct} from "constructs";
+
+export class AppPipeline extends Stack {
+    constructor(scope: Construct, id: string, props: any) {
+        super(scope, id);
+    }
+}
