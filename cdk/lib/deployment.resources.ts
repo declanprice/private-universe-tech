d745a452a994e7ab4e088cfb56423fc6a82b0373
Declan Price <declanprice@gmail.com>
2024-07-31 02:05:04 +1000

    user data to install code deploy

--- cdk/lib/deployment.resources.ts
+++ cdk/lib/deployment.resources.ts
@@ -29,7 +29,9 @@ export class DeploymentResources extends Construct {
           roleName: "PrivateUniverseServerDeploymentRole",
           assumedBy: new AnyPrincipal(),
           managedPolicies: [
-            ManagedPolicy.fromAwsManagedPolicyName("AWSCodeDeployRole"),
+            ManagedPolicy.fromAwsManagedPolicyName(
+              "service-role/AWSCodeDeployRole",
+            ),
           ],
         }),
       },
