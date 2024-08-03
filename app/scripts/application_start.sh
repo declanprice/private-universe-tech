1940cdb6ebaf61f601ff34aef249882ed7e04345
Declan Price <declanprice@gmail.com>
2024-08-03 17:43:39 +1000

    add rhel to prisma

--- app/scripts/application_start.sh
+++ app/scripts/application_start.sh
@@ -8,8 +8,6 @@ echo NEXTAUTH_URL=$(aws ssm get-parameter --name /private-universe-tech/auth/url
 echo NEXTAUTH_SECRET=$(aws ssm get-parameter --name /private-universe-tech/auth/secret --region ap-southeast-2 --query "Parameter.Value" --with-decryption) >> .env
 echo DATABASE_URL=$(aws ssm get-parameter --name /private-universe-tech/database/connection-string --region ap-southeast-2 --query "Parameter.Value" --with-decryption) >> .env
 
-export DATABASE_URL=$(aws ssm get-parameter --name /private-universe-tech/database/connection-string --region ap-southeast-2 --query "Parameter.Value" --with-decryption)
-
 npx prisma generate
 
 npx pm2 start "./node_modules/next/dist/bin/next start"
