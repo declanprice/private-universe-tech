00cf5a69c625b1287cea50f3cd62376a5d8c874d
Declan Price <declanprice@gmail.com>
2024-08-03 12:17:44 +1000

    generate prisma client as part of deployment

--- app/scripts/application_start.sh
+++ app/scripts/application_start.sh
@@ -2,11 +2,11 @@ echo "starting application"
 
 cd /var/app
 
-rm .env.production
+rm .env
 
-echo NEXTAUTH_URL=$(aws ssm get-parameter --name /private-universe-tech/auth/url --region ap-southeast-2 --query "Parameter.Value" --with-decryption) >> .env.production
-echo NEXTAUTH_SECRET=$(aws ssm get-parameter --name /private-universe-tech/auth/secret --region ap-southeast-2 --query "Parameter.Value" --with-decryption) >> .env.production
-echo DATABASE_URL=$(aws ssm get-parameter --name /private-universe-tech/database/connection-string --region ap-southeast-2 --query "Parameter.Value" --with-decryption) >> .env.production
+echo NEXTAUTH_URL=$(aws ssm get-parameter --name /private-universe-tech/auth/url --region ap-southeast-2 --query "Parameter.Value" --with-decryption) >> .env
+echo NEXTAUTH_SECRET=$(aws ssm get-parameter --name /private-universe-tech/auth/secret --region ap-southeast-2 --query "Parameter.Value" --with-decryption) >> .env
+echo DATABASE_URL=$(aws ssm get-parameter --name /private-universe-tech/database/connection-string --region ap-southeast-2 --query "Parameter.Value" --with-decryption) >> .env
 
 npx prisma generate
 
