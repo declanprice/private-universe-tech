4c391be39c23a54e961e9832309504457b617002
Declan Price <declanprice@gmail.com>
2024-08-04 19:04:47 +1000

    restricting iam permissions

--- cdk/lib/deployment.resources.ts
+++ cdk/lib/deployment.resources.ts
@@ -1,66 +1,31 @@
-import { Construct } from "constructs";
-import {
-  InstanceTagSet,
-  ServerApplication,
-  ServerDeploymentConfig,
-  ServerDeploymentGroup,
-} from "aws-cdk-lib/aws-codedeploy";
-import {
-  AnyPrincipal,
-  Effect,
-  ManagedPolicy,
-  PolicyDocument,
-  PolicyStatement,
-  Role,
-} from "aws-cdk-lib/aws-iam";
+import { Construct } from 'constructs'
+import { InstanceTagSet, ServerApplication, ServerDeploymentConfig, ServerDeploymentGroup } from 'aws-cdk-lib/aws-codedeploy'
+import { AnyPrincipal, ManagedPolicy, Role } from 'aws-cdk-lib/aws-iam'
 
 export class DeploymentResources extends Construct {
-  constructor(scope: Construct, id: string) {
-    super(scope, id);
+    constructor(scope: Construct, id: string) {
+        super(scope, id)
 
-    const application = new ServerApplication(
-      this,
-      "PrivateUniverseServerApplication",
-      {
-        applicationName: "PrivateUniverseServerApplication",
-      },
-    );
+        const application = new ServerApplication(this, 'PrivateUniverseServerApplication', {
+            applicationName: 'PrivateUniverseServerApplication',
+        })
 
-    const group = new ServerDeploymentGroup(
-      this,
-      "PrivateUniverseServerDeploymentGroup",
-      {
-        application: application,
-        deploymentConfig: ServerDeploymentConfig.fromServerDeploymentConfigName(
-          this,
-          "DeploymentConfig",
-          "CodeDeployDefault.AllAtOnce",
-        ),
-        deploymentGroupName: "PrivateUniverseServerDeploymentGroup",
-        ec2InstanceTags: new InstanceTagSet({
-          deployment: ["PrivateUniverse"],
-        }),
-        role: new Role(this, "PrivateUniverseServerDeploymentRole", {
-          roleName: "PrivateUniverseServerDeploymentRole",
-          assumedBy: new AnyPrincipal(),
-          managedPolicies: [
-            ManagedPolicy.fromAwsManagedPolicyName(
-              "service-role/AWSCodeDeployRole",
+        const group = new ServerDeploymentGroup(this, 'PrivateUniverseServerDeploymentGroup', {
+            application: application,
+            deploymentConfig: ServerDeploymentConfig.fromServerDeploymentConfigName(
+                this,
+                'DeploymentConfig',
+                'CodeDeployDefault.AllAtOnce'
             ),
-          ],
-          inlinePolicies: {
-            policy: new PolicyDocument({
-              statements: [
-                new PolicyStatement({
-                  effect: Effect.ALLOW,
-                  resources: ["*"],
-                  actions: ["*"],
-                }),
-              ],
+            deploymentGroupName: 'PrivateUniverseServerDeploymentGroup',
+            ec2InstanceTags: new InstanceTagSet({
+                deployment: ['PrivateUniverse'],
             }),
-          },
-        }),
-      },
-    );
-  }
+            role: new Role(this, 'PrivateUniverseServerDeploymentRole', {
+                roleName: 'PrivateUniverseServerDeploymentRole',
+                assumedBy: new AnyPrincipal(),
+                managedPolicies: [ManagedPolicy.fromAwsManagedPolicyName('service-role/AWSCodeDeployRole')],
+            }),
+        })
+    }
 }
