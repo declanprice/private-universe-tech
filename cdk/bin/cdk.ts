514379a5d05317f8e70a29dcb01286e1de0a811e
Declan Price <declanprice@gmail.com>
2024-08-05 12:42:04 +1000

    remove account from cdk code

--- cdk/bin/cdk.ts
+++ cdk/bin/cdk.ts
@@ -1,13 +1,9 @@
 #!/usr/bin/env node
-import "source-map-support/register";
-import * as cdk from "aws-cdk-lib";
-import { AppPipeline } from "../lib/app.pipeline";
-const app = new cdk.App();
+import 'source-map-support/register'
+import * as cdk from 'aws-cdk-lib'
+import { AppPipeline } from '../lib/app.pipeline'
+const app = new cdk.App()
 
-new AppPipeline(app, "PrivateUniversePipeline", {
-  stackName: "PrivateUniversePipeline",
-  env: {
-    region: "ap-southeast-2",
-    account: "518424097895",
-  },
-});
+new AppPipeline(app, 'PrivateUniversePipeline', {
+    stackName: 'PrivateUniversePipeline',
+})
--- cdk/lib/app.stack.ts
+++ cdk/lib/app.stack.ts
@@ -1,26 +1,21 @@
-import { Stack, StackProps} from "aws-cdk-lib";
-import { Construct } from "constructs";
-import {ComputeResources} from "./compute.resources";
-import {DeploymentResources} from "./deployment.resources";
-import {Environment} from "./environment";
+import { Stack, StackProps } from 'aws-cdk-lib'
+import { Construct } from 'constructs'
+import { ComputeResources } from './compute.resources'
+import { DeploymentResources } from './deployment.resources'
+import { Environment } from './environment'
 
 type AppStackProps = {
-    environment: Environment;
-} & StackProps;
+    environment: Environment
+} & StackProps
 
 export class AppStack extends Stack {
     constructor(scope: Construct, id: string, props: AppStackProps) {
         super(scope, id, {
             stackName: 'PrivateUniverseStack',
-            env: {
-                region: 'ap-southeast-2',
-                account: '518424097895'
-            }
-        });
+        })
 
-        new ComputeResources(this, 'ComputeResources', props);
+        new ComputeResources(this, 'ComputeResources', props)
 
-        new DeploymentResources(this, 'DeploymentResources');
+        new DeploymentResources(this, 'DeploymentResources')
     }
 }
-
